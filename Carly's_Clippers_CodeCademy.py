11096aaaa1af89945bacbf4b9ae659429f598d55
Luis Espinal <espinal1@nationwide.com>
2022-07-18 20:52:41 -0400

    Start of project, setting list and price

---
+++ Carly's_Clippers_CodeCademy.py
@@ -0,0 +1,13 @@
+# Creating list of hairstyles
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+# Creating list of prices
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+# The number of purchases for each hairstyle last week
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+  total_price += price
